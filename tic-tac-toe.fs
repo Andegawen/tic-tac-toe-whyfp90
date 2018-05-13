37522936c571fbc9885b080b3ebe67b12f8a0402
krzysztof.kroczak <krzysztof.jan.kroczak@gmail.com>
2018-05-13 18:07:20 +0100

    implementation of prune/maximize/simple evaluate

--- tic-tac-toe.fs
+++ tic-tac-toe.fs
@@ -6,8 +6,7 @@ type Board = {X:int; Y:int}
 type Player = Nought|Cross
 type Cell = {X:int; Y:int; Value:Player option}
 type Position = seq<Cell>
-
-type Node<'a> = {value:'a; sub:seq<'a Node>}
+type Node<'a> = {value:'a; sub:seq<Node<'a>>}
 
 let rootPosition (board:Board) : Position = 
     let cells = seq{ for x in 0 .. board.X-1 do
@@ -34,35 +33,31 @@ let moves (position:Position) : seq<Position> =
             })
 
     
-let rec reptree (func:Position->seq<Position>) (position:Position) : Node<Position> = 
-    {value=position;sub=(func position |> Seq.map (fun p-> reptree func p))}
+let rec reptree (f:Position->seq<_>) (position:Position) : Node<_> = 
+    {value=position;sub=(f position |> Seq.map (fun p-> reptree f p))}
+
 
 let gametree p = reptree moves p
 
 
-//the simplest 1 - won; -1: lost ; 0 - nothing ---> assume that always 3 consecutive marks means win
 let hasWon (cells:list<Cell>) : bool = 
-    printfn "%A" cells
     let distance c1 c2 : int=Convert.ToInt32((Math.Sqrt(Math.Pow(float (c2.X-c1.X), 2.0) + Math.Pow(float (c2.Y-c1.Y), 2.0))))
     
     let rec maxContinuesDistanceOnLine' cellsOnLine (acc:list<int>) = 
         match (cellsOnLine,acc) with
-        | ([_],_) -> 
-                    printfn "acc: %A" acc
-                    acc
+        | ([_],_) -> acc
         | (c1::c2::rest,acc) -> let el = List.last acc
                                 let tail = acc.[.. acc.Length-1]
                                 if distance c1 c2 = 1 
                                 then maxContinuesDistanceOnLine' (c2::rest) (tail @ [el+1])
                                 else maxContinuesDistanceOnLine' (c2::rest) (tail @ [el; 0])
 
-    let maxContinuesDistanceOnLine cellsOnLine : list<int> =
+    let maxContinuesDistanceOnLine cellsOnLine : int list =
         maxContinuesDistanceOnLine' cellsOnLine [0]
     
-    let safeListMax list = 
-        match list with
+    let safeListMax = function
         | [] -> 0
-        | a -> a |> List.max
+        | a -> List.max a
 
     let maxLineConsecutiveMarks groupLine cells = 
         cells 
@@ -72,16 +67,14 @@ let hasWon (cells:list<Cell>) : bool =
         
     
     let verticals = cells |> maxLineConsecutiveMarks (fun c->c.X)
-    printfn "%A" verticals
     let horizontals = cells |> maxLineConsecutiveMarks (fun c->c.Y)
-    printfn "%A" horizontals
     //a=1; -> so not important b=y-x
     let diagonals = cells |> maxLineConsecutiveMarks (fun c->c.Y-c.X)
-    printfn "%A" diagonals
     let m = [verticals;horizontals;diagonals] |> List.max
-    m=2 // 3 points create 2 distances x - x - x
+    m=2 // 3 points (x) create distance (-) of 2 x - x - x
 
-let staticEval (position:Position) : int = 
+///the simplest 1 - won; -1: lost ; 0 - nothing ---> assume that always 3 consecutive marks means win
+let staticEval (position:Position) = 
     let notEmptyCells = cellWithMoves position
     let hasNoughtsWon = notEmptyCells |> List.filter (fun c->c.Value = Some Nought) |> hasWon
     let hasCrossesWon = notEmptyCells |> List.filter (fun c->c.Value = Some Cross) |> hasWon
@@ -93,14 +86,27 @@ let staticEval (position:Position) : int =
 
 //Node<Position> -> Node<int> where int is computed from staticEval
 //maptree f= foldtree (Node. f) Cons Nil
-let rec maptree (func:('a->'b)) (tree:Node<'a>) : (Node<'b>) = 
+let rec maptree (func:('a->'b)) (tree: 'a Node) = 
     {value=(func tree.value); sub=tree.sub |> Seq.map (maptree func)}
+ 
 
+let rec maximize = function
+    | node when Seq.isEmpty node.sub -> node
+    | node -> node.sub |> Seq.map minimize |> Seq.maxBy (fun x->x.value)
+and minimize = function
+    | node when Seq.isEmpty node.sub -> node
+    | node -> node.sub |> Seq.map maximize |> Seq.minBy (fun x->x.value)
 
+let rec prune (limit:int) (node:'a Node) =
+    match limit with
+    | 0 -> {node with sub=Seq.empty}
+    | l -> {node with sub=node.sub |> Seq.map (fun x-> prune (l-1) x)}
 
-maptree staticEval (gametree (rootPosition {X=3;Y=3}))
+let evaluate position =
+    position  
+    |> gametree 
+    |> prune 5
+    |> maptree staticEval
+    |> maximize
 
-// let maximize (Node n sub) = max (map minimize sub)
-// let minimize (Node n sub) = min (map maximize sub)
-// let evaluate = maximize 
-
+let e0 = rootPosition {X=3;Y=3} |> evaluate
