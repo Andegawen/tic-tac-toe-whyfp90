0bdbb7c5837302c3ab1a0c773a5677f4a169c4d1
krzysztof.kroczak <krzysztof.jan.kroczak@gmail.com>
2018-05-27 15:09:35 +0100

    try to understand what's wrong by evaluating in playground

--- Playground.fsx
+++ Playground.fsx
@@ -4,9 +4,46 @@ open TicTacToeTypes
 open TicTacToe
 
 
-let rec repeat (func:(Position->seq<Position>)) a0 : seq<Position>    = 
-    let a1 = func a0;
-    Seq.collect (fun x-> repeat func x) a1
-let z = repeat moves (rootPosition {X=3;Y=3}) |> Seq.takeWhile (fun x->x=Seq.empty)
+let evaluate2 position (player:Player) =
+    position
+    |> gametree
+    |> prune 3
 
-z |> Seq.toArray |> Array.iter (fun elem -> printf "%s" (posToString elem))
+let interestingPos = seq[
+                            {X=0;Y=0;Value=Some Nought} 
+                            {X=0;Y=1;Value=Some Cross}
+                            {X=0;Y=2;Value=Some Nought}
+                            {X=1;Y=0;Value=None}
+                            {X=1;Y=1;Value=None}
+                            {X=1;Y=2;Value=None}
+                            {X=2;Y=0;Value=None}
+                            {X=2;Y=1;Value=None}
+                            {X=2;Y=2;Value=None}          
+                        ]
+interestingPos |> posToString
+let evalPos = evaluate2 interestingPos Cross;
+let evalPosValue = evalPos.value
+let ``evaluate interesting position sub x=1 y=1 Cross`` =
+    evalPos.sub 
+    |> Seq.filter (fun x->
+                        let p = x.value
+                        p|>Seq.exists (fun c->c.X=1 && c.Y=1 && c.Value = Some Cross))
+    |> Seq.head
+
+let ``evaluate interesting position sub x=2 y=1 Empty`` =
+    ``evaluate interesting position sub x=1 y=1 Cross``.sub 
+    |> Seq.filter (fun x->
+                        let p = x.value
+                        p|>Seq.exists (fun c->c.X=2 && c.Y=1 && c.Value = None))
+    |> Seq.head
+
+let ``evaluate interesting position sub x=2 y=1 Cross`` = 
+    ``evaluate interesting position sub x=2 y=1 Empty``.sub 
+    |> Seq.filter (fun x->
+                        let p = x.value
+                        p|>Seq.exists (fun c->c.X=2 && c.Y=1 && c.Value = Some Cross))
+    |> Seq.head
+
+``evaluate interesting position sub x=2 y=1 Cross``.value |> posToString
+maptree (simpleStaticEval Cross) ``evaluate interesting position sub x=2 y=1 Cross``
+//let kiki1 = kiki.sub |> Seq.filter (fun x->x.value<>0) |> Seq.toArray
