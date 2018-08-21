5e784bd41058a11e5dc1d931f891ce0020634b9f
krzysztof.kroczak <krzysztof.jan.kroczak@gmail.com>
2018-08-21 07:12:43 +0100

    Read string board as seq

--- simpleStaticEvalTests.fs
+++ simpleStaticEvalTests.fs
@@ -4,8 +4,24 @@ open Xunit
 open FsUnit.Xunit
 open Domain
 open Game
+open System
 
 
+let strToPlayer str =
+    match str with
+    | "X" 
+    | "x" -> Some Cross
+    | "O" -> Some Nought
+    | "o" -> Some Nought
+    | _ -> None
+
+let toSeq (str:String) = 
+    let rows = str.Split('\n')
+    rows 
+    |> Seq.mapi (fun y r-> let values = r.Split('|')
+                           (y, values |> Array.mapi (fun x el -> (x, strToPlayer el) )))
+    |> Seq.collect (fun (y,z)-> z|> Seq.map (fun (x,p)->{X=x;Y=y;Value=p}))
+    
 [<Fact>]
 let ``Position with no win or no lost evals to 0`` () =    
     let positionWithEmptyCell = seq{
@@ -13,38 +29,19 @@ let ``Position with no win or no lost evals to 0`` () =
                                     }
     simpleStaticEval Nought positionWithEmptyCell |> should equal 0
 
-/// x|o| |
-/// x|o| |
-///  |o| |
+
 [<Fact>]
 let ``Win evals as 1`` () = 
-    let positionWithEmptyCell = seq{
-                                    yield {X=0;Y=0;Value=Some Cross} 
-                                    yield {X=1;Y=0;Value=Some Nought}
-                                    yield {X=2;Y=0;Value=None}
-                                    yield {X=0;Y=1;Value=Some Cross}
-                                    yield {X=1;Y=1;Value=Some Nought}
-                                    yield {X=2;Y=1;Value=None}
-                                    yield {X=0;Y=2;Value=None}
-                                    yield {X=1;Y=2;Value=Some Nought}
-                                    yield {X=2;Y=2;Value=None}
-                                    }
+    let positionWithEmptyCell =   "X|O| \n"
+                                + "X|O| \n"
+                                + " |O| " |> toSeq
+    
     simpleStaticEval Nought positionWithEmptyCell |> should equal 1
 
-/// x|o|o|
-/// x|o| |
-/// x| | |
 [<Fact>]
 let ``Lost evals to -1`` ()= 
-    let positionWithEmptyCell = seq{
-                                    yield {X=0;Y=0;Value=Some Cross} 
-                                    yield {X=1;Y=0;Value=Some Nought}
-                                    yield {X=2;Y=0;Value=Some Nought}
-                                    yield {X=0;Y=1;Value=Some Cross}
-                                    yield {X=1;Y=1;Value=Some Nought}
-                                    yield {X=2;Y=1;Value=None}
-                                    yield {X=0;Y=2;Value=Some Cross}
-                                    yield {X=1;Y=2;Value=None}
-                                    yield {X=2;Y=2;Value=None}
-                                    }
+    let positionWithEmptyCell = "X|O|O\n"
+                              + "X|O| \n"
+                              + "X| | " |> toSeq
+     
     simpleStaticEval Nought positionWithEmptyCell |> should equal -1
